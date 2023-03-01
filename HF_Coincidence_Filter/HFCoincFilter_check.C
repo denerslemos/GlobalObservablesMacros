ef64564fd50014ef70aefa7d508168debeaff9d4
ddesouza <dener.lemos@cern.ch>
2023-03-01 18:28:02 +0100

    changes

--- HF_Coincidence_Filter/HFCoincFilter_check.C
+++ HF_Coincidence_Filter/HFCoincFilter_check.C
@@ -39,7 +39,7 @@ void HFCoincFilter_check(TString input_file, TString output_file){
 	int numMinHFTower2, numMinHFTower3, numMinHFTower4, numMinHFTower5; // minimum number of towers (towermaker) between HF+ and HF- for thresholds 2, 3, 4 and 5 GeV
 
 	hea_tree->SetBranchStatus("*", 0); // disable all branches - this is important while reading big files
-    hea_tree->SetBranchStatus("pprimaryVertexFilter", 1);
+        hea_tree->SetBranchStatus("pprimaryVertexFilter", 1);
     hea_tree->SetBranchAddress("pprimaryVertexFilter", &pprimaryVertexFilter);    
     hea_tree->SetBranchStatus("pclusterCompatibilityFilter", 1);
     hea_tree->SetBranchAddress("pclusterCompatibilityFilter", &pclusterCompatibilityFilter);    
@@ -156,6 +156,37 @@ void HFCoincFilter_check(TString input_file, TString output_file){
 	TH1D *hist_hibin_PF4Th5 = new TH1D("hist_hibin_PF4Th5","hist_hibin_PF4Th5",201,0.0,201.0);
 	hist_hibin_PF4Th5->Sumw2();
 
+        TH1D *hist_hibin_PFTh1 = new TH1D("hist_hibin_PFTh1","hist_hibin_PFTh1",201,0.0,201.0);
+        hist_hibin_PFTh1->Sumw2();
+        TH1D *hist_hibin_PFTh2 = new TH1D("hist_hibin_PFTh2","hist_hibin_PFTh2",201,0.0,201.0);
+        hist_hibin_PFTh2->Sumw2();
+        TH1D *hist_hibin_PFTh3 = new TH1D("hist_hibin_PFTh3","hist_hibin_PFTh3",201,0.0,201.0);
+        hist_hibin_PFTh3->Sumw2();
+        TH1D *hist_hibin_PFTh4 = new TH1D("hist_hibin_PFTh4","hist_hibin_PFTh4",201,0.0,201.0);
+        hist_hibin_PFTh4->Sumw2();
+        TH1D *hist_hibin_PFTh5 = new TH1D("hist_hibin_PFTh5","hist_hibin_PFTh5",201,0.0,201.0);
+        hist_hibin_PFTh5->Sumw2();
+        TH1D *hist_hibin_PFTh10 = new TH1D("hist_hibin_PFTh10","hist_hibin_PFTh10",201,0.0,201.0);
+        hist_hibin_PFTh10->Sumw2();
+        TH1D *hist_hibin_PFTh15 = new TH1D("hist_hibin_PFTh15","hist_hibin_PFTh15",201,0.0,201.0);
+        hist_hibin_PFTh15->Sumw2();
+        TH1D *hist_hibin_PFTh20 = new TH1D("hist_hibin_PFTh20","hist_hibin_PFTh20",201,0.0,201.0);
+        hist_hibin_PFTh20->Sumw2();
+        TH1D *hist_hibin_PFTh25 = new TH1D("hist_hibin_PFTh25","hist_hibin_PFTh25",201,0.0,201.0);
+        hist_hibin_PFTh25->Sumw2();
+        TH1D *hist_hibin_PFTh30 = new TH1D("hist_hibin_PFTh30","hist_hibin_PFTh30",201,0.0,201.0);
+        hist_hibin_PFTh30->Sumw2();
+        TH1D *hist_hibin_PFTh35 = new TH1D("hist_hibin_PFTh35","hist_hibin_PFTh35",201,0.0,201.0);
+        hist_hibin_PFTh35->Sumw2();
+        TH1D *hist_hibin_PFTh40 = new TH1D("hist_hibin_PFTh40","hist_hibin_PFTh40",201,0.0,201.0);
+        hist_hibin_PFTh40->Sumw2();
+        TH1D *hist_hibin_PFTh45 = new TH1D("hist_hibin_PFTh45","hist_hibin_PFTh45",201,0.0,201.0);
+        hist_hibin_PFTh45->Sumw2();
+        TH1D *hist_hibin_PFTh50 = new TH1D("hist_hibin_PFTh50","hist_hibin_PFTh50",201,0.0,201.0);
+        hist_hibin_PFTh50->Sumw2();
+        TH1D *hist_hibin_PFTh60 = new TH1D("hist_hibin_PFTh60","hist_hibin_PFTh60",201,0.0,201.0);
+        hist_hibin_PFTh60->Sumw2();
+
 	// loop over events
 	int nevents = hea_tree->GetEntries(); // number of events
 	cout << endl;
@@ -229,6 +260,12 @@ void HFCoincFilter_check(TString input_file, TString output_file){
 		if(hiHF_pfle > 5 && min(nCountsHFPlus_pf, nCountsHFMinus_pf) > 2){hist_hibin_PF3Th5->Fill(hiBin);}
 		if(hiHF_pfle > 5 && min(nCountsHFPlus_pf, nCountsHFMinus_pf) > 3){hist_hibin_PF4Th5->Fill(hiBin);}
 
+                if(hiHF_pfle > 1){hist_hibin_PFTh1->Fill(hiBin);}
+                if(hiHF_pfle > 2){hist_hibin_PFTh2->Fill(hiBin);}
+                if(hiHF_pfle > 3){hist_hibin_PFTh3->Fill(hiBin);}
+                if(hiHF_pfle > 4){hist_hibin_PFTh4->Fill(hiBin);}
+                if(hiHF_pfle > 5){hist_hibin_PFTh5->Fill(hiBin);}
+
 	}
 
 	// Open, write and close the output file
@@ -278,6 +315,13 @@ void HFCoincFilter_check(TString input_file, TString output_file){
 	hist_hibin_PF2Th5->Write();
 	hist_hibin_PF3Th5->Write();
 	hist_hibin_PF4Th5->Write();
+
+	hist_hibin_PFTh1->Write();
+        hist_hibin_PFTh2->Write();
+        hist_hibin_PFTh3->Write();
+        hist_hibin_PFTh4->Write();
+        hist_hibin_PFTh5->Write();
+
 	MyFile->Close();
 
 	cout << endl;
